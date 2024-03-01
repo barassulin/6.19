704b4370285e7b168a504d50279bd62d518f85df
barassulin <148526209+barassulin@users.noreply.github.com>
2024-03-01 23:04:28 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
 from scapy.all import *
+from scapy.layers.inet import TCP, IP
 
-Timeout=0.5
+Timeout = 0.5
 PORT = 20
-CIP = input("enter ip")
 
-while PORT<1024:
-    syn_segment = TCP(dport=PORT, seq=123, flags='S')
-    syn_packet = IP(dst=CIP)/syn_segment
-    syn_ack_packet = sr1(syn_packet, timeout = Timeout)
-    F = syn_ack_packet['TCP'].flags  # this should give you an integer
-    if F & SYN and F & ACK:
-        print("port: " + PORT)
-    else:
-        print("closed")
-    PORT=PORT+1
+
+def main():
+    cip = input("enter ip")
+    while PORT < 1024:
+        syn_segment = TCP(dport=PORT, seq=123, flags='S')
+        syn_packet = IP(dst=cip) / syn_segment
+        syn_ack_packet = sr1(syn_packet, timeout=Timeout)
+        if syn_ack_packet[TCP].flags & TCP.SYN == TCP.SYN and \
+                syn_ack_packet[TCP].flags & TCP.ACK == TCP.ACK:
+            print("port: " + PORT)
+        else:
+            print("closed")
+        PORT = PORT + 1
+
+
+if __name__ == "__main__":
+    main()
