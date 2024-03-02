4189714917fc075c41d31e002a4d3f0044ce118e
barassulin <148526209+barassulin@users.noreply.github.com>
2024-03-02 12:08:19 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,44 @@
+"""
+
+Author: Bar Assulin
+Date: 2/3/24
+
+Description:
+This program gets an ip from the client and finds if the ports are open or not.
+
+"""
+
+# imports:
 from scapy.all import *
 from scapy.layers.inet import TCP, IP
 
+# constants:
 Timeout = 0.5
-PORT = 20
+SYN = 0x02
+ACK = 0x10
+STARTING_PORT = 20
+ENDING_PORT = 1025
 
 
+# funcs:
 def main():
     cip = input("enter ip")
-    while PORT < 1024:
-        syn_segment = TCP(dport=PORT, seq=123, flags='S')
+    port = STARTING_PORT
+    while port < ENDING_PORT:
+        syn_segment = TCP(dport=port, seq=123, flags='S')
         syn_packet = IP(dst=cip) / syn_segment
         syn_ack_packet = sr1(syn_packet, timeout=Timeout)
-        if syn_ack_packet[TCP].flags & TCP.SYN == TCP.SYN and \
-                syn_ack_packet[TCP].flags & TCP.ACK == TCP.ACK:
-            print("port: " + PORT)
+        if syn_ack_packet is not None:
+            try:
+                if syn_ack_packet['TCP'].flags & SYN and syn_ack_packet['TCP'].flags & ACK:
+                    print("port: ", port, " is open")
+                else:
+                    print("port: ", port, " is closed")
+            except Exception as err:
+                print("error: ", err)
         else:
-            print("closed")
-        PORT = PORT + 1
+            print("port: ", port, " is not responding")
+        port = port+1
 
 
 if __name__ == "__main__":
